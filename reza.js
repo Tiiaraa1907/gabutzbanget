5d870ffcc3750c7777735a821821c862ccc94e47
Tiiaraa1907 <72679988+Tiiaraa1907@users.noreply.github.com>
2021-01-31 07:17:42 -0800

    hai
    commit apaan ya

---
+++ reza.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>HEYO!!</title>
+</head>
+<body>
+    <script src="reza.js"></script>
+</body>
+</html>

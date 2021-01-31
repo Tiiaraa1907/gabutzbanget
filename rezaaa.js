f5928e55d058a28e241c891e3c186bd44d1f16db
Tiiaraa1907 <72679988+Tiiaraa1907@users.noreply.github.com>
2021-01-31 07:18:27 -0800

    Create rezaaa.js

---
+++ rezaaa.js
@@ -0,0 +1,15 @@
+nama = prompt('kenalan yuk! (ayo/tidak..pakai huruf kecil)');
+
+if (nama == "ayo"){
+    gas= prompt('siapa namamu? ');
+    alert("hai " + gas);
+    alert("I LOVE YOUUUUUUUUUUU");
+    prompt("jawab dong :'(");
+    alert("SEKIAN. TERIMAKSIH SAYANGGGG")
+}
+else if (nama=="tidak"){
+    alert('oke gajadi:)');
+}
+else{
+    alert('gabisa baca tulisan ihhh');
+}
